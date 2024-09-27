4c1321e683db985016725b03d1783976c4f2c05f
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-27 14:43:59 +0200

    feat(navbar): create navbar with different buttons and dropdown
    For the moment, the navbar didn't work, it's just the front

--- angular_front/src/app/app.module.ts
+++ angular_front/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
-})
-export class AppModule { }
+    declarations: [
+    ],
+    imports: [
+        CommonModule,
+        NavbarComponent
+    ]
+  })
+  export class AppModule { }
