b7b6eaca5d5fbbed0991688ef751aafbc338204a
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-10-03 17:33:15 +0200

    feat(Firebase): setup Firebase authentification
    I succeeded to implement Firebase authentification with Google

--- angular_front/src/app/pages/login/login.component.ts
+++ angular_front/src/app/pages/login/login.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+// signin.component.ts
+import { Component } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.scss",
 })
 export class LoginComponent {
-
+  constructor(public angularFireAuth: AngularFireAuth) {}
+  logOut() {
+    this.angularFireAuth.signOut();
+  }
 }
---
+++ angular_front/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDwCXWkSH351eyyyBfdMm1LEJ_6zKPABN4",
+        authDomain: "tr3llo.firebaseapp.com",
+        projectId: "tr3llo",
+        storageBucket: "tr3llo.appspot.com",
+        messagingSenderId: "317114157318",
+        appId: "1:317114157318:web:fae17481edf388a98d3687",
+        measurementId: "G-ZNZWLMBT31"
+    }
+};
